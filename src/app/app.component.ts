0f6b843c21f8f52c0437ae88e34d1c9298e61172
Shane02Nathalie <shane04nathalie@gmail.com>
2022-01-07 11:40:29 +0700

    commiting calcu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'calculator';
+  title = 'Calculator Project of Shane Nathalie Acebo';
+
+  public num1:number=0;
+  public num2:number=0;
+  public num3:number=0;
+
+  add(){
+    this.num3=this.num1+this.num2;
+  }
+  
+  subtract(){
+    this.num3=this.num1-this.num2;
+  }
+  
+  multiply(){
+    this.num3=this.num1*this.num2;
+  }
+  
+  divide(){
+    this.num3=this.num1/this.num2;
+  }
 }
